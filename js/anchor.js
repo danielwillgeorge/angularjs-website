9bfdf60310953a0eeaf4c67a3b053bb1f763831b
Daniel George <daniel.will.george@gmail.com>
2015-08-07 19:03:41 -0700

    first commit

---
+++ js/anchor.js
@@ -0,0 +1,23 @@
+(function scrollTo(adops) {
+	
+	location.adops = "#" + adops;
+
+});
+
+(function scrollTo(datarep) {
+	
+	location.datarep = "#" + datarep;
+
+});
+
+(function scrollTo(brandid) {
+	
+	location.brandid = "#" + brandid;
+
+});
+
+(function scrollTo(rightsid) {
+	
+	location.rightsid = "#" + rightsid;
+
+});    
